7c37f855bc746956aa539667a8a7aae4c15400f3
Joshua Mack <mackncheesiest@gmail.com>
2023-05-26 13:56:24 -0700

    Updated bash_functions.sh

--- bash_functions.sh
+++ bash_functions.sh
@@ -2,5 +2,11 @@ peta-docker() {
   VERSION=$1
   other_args=$(echo "${@:2}" | tr -d '"')
   
-  sudo docker run --rm -it --volume /:/host-fs --workdir /host-fs/$(pwd) petalinux:${VERSION} /bin/bash -i -c "${other_args}"
+  sudo docker run --rm -it --volume /:/host-fs --workdir /host-fs/$(pwd) --network host petalinux:${VERSION} /bin/bash -i -c "${other_args}"
+}
+
+peta-docker-interactive() {
+  VERSION=$1
+  
+  sudo docker run --rm -it --volume /:/host-fs --workdir /host-fs/$(pwd) --network host petalinux:${VERSION} /bin/bash
 }
